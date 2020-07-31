0ddb21d91789ab6573efb0af09877a9b0f489e57
Garrett Bass <garrettbass@macbook-pro.attlocal.net>
2020-07-31 08:44:43 -0500

    7/31 commit

---
+++ socket-backend/server.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.send('hello!'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('message', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast', msg);
+    });
+});
+    
+http.listen(8000, () => {
+  console.log('listening on *:8000');
+});
